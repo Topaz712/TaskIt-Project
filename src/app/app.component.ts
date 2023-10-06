ad9ff74ed6a14c3bf1bcdad78ecd547879c3abf3
Topaz712 <tzam712@gmail.com>
2023-10-06 02:12:19 -0500

    removed some unnecessary divs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,5 @@ export class AppComponent {
   user: User[] = [
     new User('https://images.pexels.com/photos/15272229/pexels-photo-15272229/free-photo-of-black-and-white-photo-of-mountains-and-highway.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load', 'Topaz@taskit.com')
     ];
+  
 }
